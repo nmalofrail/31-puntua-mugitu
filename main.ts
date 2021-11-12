c5ab4a03f5e0d7218762365c9ee49b75c7571bb8
nmalofrail <93262210+nmalofrail@users.noreply.github.com>
2021-11-12 14:27:27 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    sprite.move(1)
+})
+input.onButtonPressed(Button.AB, function () {
+    sprite.move(1)
+})
+input.onButtonPressed(Button.B, function () {
+    sprite.move(1)
+})
+let sprite: game.LedSprite = null
+sprite = game.createSprite(2, 2)
+basic.forever(function () {
+	
+})
